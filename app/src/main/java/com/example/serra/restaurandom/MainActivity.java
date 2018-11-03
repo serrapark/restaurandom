e5a44880da1ad8172104d2ab2cd380bca1b46f03
Ji Han <jihan2311@gmail.com>
2018-11-03 16:37:29 -0700

    create popup

--- app/src/main/java/com/example/serra/restaurandom/MainActivity.java
+++ app/src/main/java/com/example/serra/restaurandom/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.serra.restaurandom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.support.v7.widget.Toolbar;
+import android.widget.PopupMenu;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
@@ -23,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     //Create action bar button
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.actionbar, menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
@@ -31,13 +34,24 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.show_filters) {
-
+            View menuItemView = findViewById(R.id.show_filters);
+            PopupMenu filterMenu = new PopupMenu(this, menuItemView);
+            filterMenu.inflate(R.menu.filter_menu);
+            filterMenu.show();
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    public void getRestaurant(View view) {
+    public void getRestaurant(View view){
 
     }
+
+//    public void showFilters(View view){
+//        PopupMenu filters = new PopupMenu(this, view);
+//        filters.inflate(R.menu, filters.getMenu());
+//        filters.show();
+//    }
+
 
 }
