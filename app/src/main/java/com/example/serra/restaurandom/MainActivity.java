b4fae77cc7b0d18d2004f78b1a121d803c1f2ea7
Ji Han <jihan2311@gmail.com>
2018-11-03 15:29:15 -0700

    create actionbar

--- app/src/main/java/com/example/serra/restaurandom/MainActivity.java
+++ app/src/main/java/com/example/serra/restaurandom/MainActivity.java
@@ -2,23 +2,42 @@ package com.example.serra.restaurandom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    Toolbar actionbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.get_restaurant);
+        actionbar = findViewById(R.id.actionbar);
+        setSupportActionBar(actionbar);
+    }
+    //Create action bar button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.show_filters) {
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void getRestaurant(View view) {
 
     }
 
-    public void showFilters(View view) {
-
-    }
 }
